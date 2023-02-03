79a6df495be2b83ad67a7c3db0a4b3432a93a665
Phan Thanh Toàn <DESKTOP-2EP69KG\ADMIN>
2023-02-03 15:17:41 +0700

    create Miscroservice

---
+++ user-server/src/main/java/com/example/UserServerApp.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class UserServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServerApp.class,args);
+    }
+}
